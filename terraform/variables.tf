42c99ee883fcf99f46b6f15515b624a1e038dcc5
lakshmanan22 <130641008+lakshmanan22@users.noreply.github.com>
2025-07-17 16:19:38 +0530

    Add files via upload

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  default     = "t2.micro"
+}
+
+variable "ami_id" {
+  description = "Amazon Machine Image ID"
+  default     = "ami-0a1235697f4afa8a4"  # Amazon Linux 2 in mumbai
+}
