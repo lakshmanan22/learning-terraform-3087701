2c20e351818a09045c39492569d242bea684f619
lakshmanan22 <130641008+lakshmanan22@users.noreply.github.com>
2025-07-17 12:34:33 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,11 +1,23 @@
 terraform {
+  cloud {
+    organization = "Demo-3i"
+
+    workspaces {
+      name = "terraform"
+    }
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
+
+  required_version = ">= 1.5.0"
 }
 
+
 provider "aws" {
-  region  = "us-west-2"
+  region = var.aws_region
 }
